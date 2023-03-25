668420963a5bd650b68a9aa12f5f6c6ecb413e7f
bmchun00 <bmchun00@korea.ac.kr>
2023-03-25 03:41:23 +0000

    Update main.dart

--- widget_example/lib/main.dart
+++ widget_example/lib/main.dart
@@ -6,110 +6,83 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const String _title = "Widget (Stateful)";
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: _title,
+      home: WidgetApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class WidgetApp extends StatefulWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _WidgetExampleState createState() => _WidgetExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _WidgetExampleState extends State<WidgetApp> {
+  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems = new List.empty(growable: true);
+  String? _buttonText;
+  String sum="";
+  TextEditingController val1 = TextEditingController();
+  TextEditingController val2 = TextEditingController();
+  @override
+  void initState(){
+    super.initState();
+    for(var item in _buttonList){
+      _dropDownMenuItems.add(DropdownMenuItem(value: item, child: Text(item),));
+    }
+    _buttonText = _dropDownMenuItems[0].value;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Widget Example')
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(padding: EdgeInsets.all(30),child: Text('결과 : $sum', style: TextStyle(fontSize: 30),),),
+              Padding(padding: EdgeInsets.all(30),child: TextField(
+                keyboardType: TextInputType.number, controller: val1,
+              ),),
+              Padding(padding: EdgeInsets.all(30),child: TextField(
+                keyboardType: TextInputType.number, controller: val2,
+              ),),
+              Padding(padding: EdgeInsets.all(30),child: ElevatedButton(onPressed: (){
+                setState(() {
+                  int res = int.parse(val1.value.text)+int.parse(val2.value.text);
+                  sum = '$res';
+                });
+              }, child: Row(
+                children: [
+                  Icon(Icons.add), Text("더하기")
+                ],
+              ),
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber)),
+              ),
+              ),
+              Padding(padding: EdgeInsets.all(20),
+              child: DropdownButton(
+                value: _buttonText,
+                items: _dropDownMenuItems, onChanged: (String? value){
+                setState(() {
+                  _buttonText = value;
+                },);
+              },),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
